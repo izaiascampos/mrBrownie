1ccd5e248568a6655e30b1360ee92cb5229fa525
izaiascampos <izaiasrcampos3@hotmail.com>
2021-12-15 17:46:34 -0300

    primeiro commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+window.sr = ScrollReveal({reset:true});
+
+sr.reveal('.banner--text', {
+    duration:2000,
+    rotate: {x:20, y:20, z:0}
+});
+sr.reveal('.flavors--desc--area', {
+    duration:2000,
+    rotate: {x:20, y:20, z:0}
+});
+sr.reveal('.social--area', {
+    duration:2000,
+    rotate: {x:20, y:20, z:0}
+});
+
